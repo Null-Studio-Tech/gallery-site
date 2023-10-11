9a5a8d7b19daed203c852a290751a8b5b5a04ad1
dongzhigang <dongzhigang@megvii.com>
2023-10-11 16:56:11 +0800

    chore: 初始化工程

---
+++ src/components/client/Counter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const onClickAdd = () => {
+  counter.value++;
+}
+</script>
+
+<template>
+  <p class="text-white">
+    count: <span>{{ counter }}</span>
+  </p>
+  <button class="text-white border border-solid border-white px-3" @click="onClickAdd">add</button>
+</template>
