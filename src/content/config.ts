5aee929edf1a5e5c7d2b6026c4bfd50f61b1101b
董志刚 <dongzhigang_bit@126.com>
2023-11-15 00:40:49 +0800

    chore: merge

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,3 @@
-// 1. 从 `astro:content` 导入
-import { z, defineCollection } from 'astro:content';
-// 2. 定义集合
-const workCollection = defineCollection({
-  schema: {
-    title: z.string(),
-    author: z.string().optional(),
-    references: z.array(z.string())
-  }
-});
-// 3. 导出一个 `collections` 对象来注册集合
-//    这个键应该与 `src/content` 中的集合目录名匹配
-export const collections = {
-  works: workCollection
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb84a3f43666eb82e91a0a99eb4bc0133b9f0182cec0d9ced3ab6dff8d676d95
+size 454
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'laptop': { max: '1316px' },
+			'mobile': { max: '600px' },
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			keyframes: {
+				'highlight-normal': {
+					'from': { 'background-position': '0px' },
+					'to': { 'background-position': '-3400px' }
+				},
+				'highlight-small': {
+					'from': { 'background-position': '0px' },
+					'to': { 'background-position': '-1360px' }
+				}
+			},
+			animation: {
+				'highlight-normal': 'highlight-normal .5s steps(17)',
+				'highlight-small': 'highlight-small .5s steps(17)'
+			}
+		},
 	},
 	plugins: [],
 }
