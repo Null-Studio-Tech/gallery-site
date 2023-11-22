b2e54e01cf49d7cf94ac299c8ae472a1b68b57f6
luckykai <1548051084@qq.com>
2023-11-23 01:57:09 +0800

    优化内置样式

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: [
+		'col-span-12',
+		'mb-36',
+		'mb-24',
+		'mt-24',
+		'mb-11',
+		'text-center',
+		'col-span-2',
+		'col-span-4',
+		'col-span-6',
+		'col-span-8',
+		'col-span-10'
+  ],
 	theme: {
 		screens: {
 			'laptop': { max: '1316px' },
