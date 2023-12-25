8886ba150d902fc7c5819a75e81a3f208536be10
tangkai <tangkai@megvii.com>
2023-12-25 13:55:24 +0800

    1920->2000

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro-imagetools/config";
 export default defineConfig({
   placeholder: "dominantColor",
   aspect: 1.77777777777777,
-  breakpoints: [800, 1200, 1600, 1920]
+  breakpoints: [800, 1200, 1600, 2000]
 })
